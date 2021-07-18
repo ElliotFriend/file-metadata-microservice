f1f14994c342dd542d6402e1b8f84ff2f2d65acd
Elliot J. Voris <elliot@voris.me>
2021-07-17 23:47:23 -0500

    I think I got it all working...
    This almost seems too simple... What's the catch??!
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+let app = express();
+let storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.fieldname)
+  }
+})
+let upload = multer({ storage: storage })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+})
 
 
 const port = process.env.PORT || 3000;
